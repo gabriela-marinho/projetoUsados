acc5eb4eb33d9b4c2712fa20fefd3aaf28a56b61
@GabrielaBMarinho <g.barreto.marinho@gmail.com>
2022-11-04 11:32:34 -0300

    reactive dom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import Busca from './Components/Shared/Header/Busca/Busca';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
     <div >
-     
+     <Header></Header>
+     <Busca></Busca>
       <h1> oi</h1>
     </div>
   );
