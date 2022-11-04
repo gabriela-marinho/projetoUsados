6cd903935c19c3d9cbfe18fb778f4e6775807fdd
@GabrielaBMarinho <g.barreto.marinho@gmail.com>
2022-11-04 13:36:11 -0300

    routes dom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Busca from './Components/Shared/Header/Busca/Busca';
 import Header from './Components/Shared/Header/Header';
+import Home from './Pages/Home/Home';
+import Cadastro from './Pages/Cadastro/Cadastro';
+
+import {Routes,Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div >
      <Header></Header>
      <Busca></Busca>
+     <Routes>
+        <Route path='/' element={Home}></Route>
+        <Route path='/cadastro' element={Cadastro}></Route>
+     </Routes>
       <h1> oi</h1>
     </div>
   );
